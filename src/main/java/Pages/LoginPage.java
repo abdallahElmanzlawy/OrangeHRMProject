40ad7f99d6bf099c944aa8527c6e033d10c6ab56
Abdallah Elmanzlawy <abdallahelmanzlawy2001@gmail.com>
2025-05-24 19:22:46 +0300

    New Commit

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -2,20 +2,24 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class LoginPage {
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
     //locatedElements
-    By userNameField = By.cssSelector("input[name='username']");
-    By passwordField = By.cssSelector("input[name='password']");
-    By loginButton = By.cssSelector("button[type='submit']");
-    By verifyResult = By.xpath("//*[@id='app']/div[1]/div[1]/header/div[1]/div[1]/span/h6");
+    private final By userNameField = By.cssSelector("input[name='username']");
+    private final By passwordField = By.cssSelector("input[name='password']");
+    private final By loginButton = By.cssSelector("button[type='submit']");
+    private final By verifySuccessResult = By.xpath("//h6[contains(.,'Dashboard')]");
+    private final By verifyInvalidResult = By.xpath("//p[contains(., 'Invalid')]");
+    private final By requiredErrorMessage = By.cssSelector(".oxd-input-field-error-message");
 
     public LoginPage(WebDriver driver)
     {
@@ -43,10 +47,43 @@ public class LoginPage {
         driver.findElement(loginButton).click();
     }
 
-    public String getResult()
+    public String getSuccessResult()
     {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(verifyResult));
-        return driver.findElement(verifyResult).getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(verifySuccessResult));
+        return driver.findElement(verifySuccessResult).getText();
+    }
+
+
+//    public String getInvalidResult()
+//    {
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(verifyInvalidResult));
+//        return driver.findElement(verifyInvalidResult).getText();
+//    }
+
+    public boolean isInvalidErrorMessageVisible()
+    {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(verifyInvalidResult));
+        return driver.findElement(verifyInvalidResult).isDisplayed();
+    }
+
+    public boolean isRequiredErrorMessageVisible()
+    {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(requiredErrorMessage));
+        return driver.findElement(requiredErrorMessage).isDisplayed();
+    }
+
+    public boolean isDuplicatedRequiredErrorMessageVisible()
+    {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(requiredErrorMessage));
+        List<WebElement> requiredErrorList = driver.findElements(requiredErrorMessage);
+        for (int i = 0 ; i < requiredErrorList.size() ; i++)
+        {
+            if ( !requiredErrorList.get(i).isDisplayed())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- src/test/java/LoginPageTests.java
+++ src/test/java/LoginPageTests.java
@@ -1,20 +1,91 @@
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class LoginPageTests extends BaseTest {
 
     @Test
     public void testSuccessLogin()
     {
-        //EnterUserName
+        //EnterValidUserName
         loginPage.setUserNameField("Admin");
-        //EnterPassword
+        //EnterValidPassword
         loginPage.setPasswordField("admin123");
         //ClickButton
         loginPage.clickLoginButton();
 
         //Assertion
-        assertEquals(loginPage.getResult(),"Dashboard","Failed");
+        assertEquals(loginPage.getSuccessResult(),"Dashboard","Failed Login");
+    }
+
+    @Test
+    public void testInvalidUsername()
+    {
+        //EnterInvalidUserName
+        loginPage.setUserNameField("Ad");
+        //EnterValidPassword
+        loginPage.setPasswordField("admin123");
+        //ClickButton
+        loginPage.clickLoginButton();
+
+        //Assertion
+        assertTrue(loginPage.isInvalidErrorMessageVisible(),"Incorrect Results");
+    }
+
+    @Test
+    public void testInvalidPassword()
+    {
+        //EnterInvalidUserName
+        loginPage.setUserNameField("Admin");
+        //EnterValidPassword
+        loginPage.setPasswordField("admin");
+        //ClickButton
+        loginPage.clickLoginButton();
+
+        //Assertion
+        assertTrue(loginPage.isInvalidErrorMessageVisible(),"Incorrect Results");
+    }
+
+    @Test
+    public void testEmptyPassword()
+    {
+        //EnterInvalidUserName
+        loginPage.setUserNameField("Admin");
+        //EnterValidPassword
+        loginPage.setPasswordField("");
+        //ClickButton
+        loginPage.clickLoginButton();
+
+        //Assertion
+        assertTrue(loginPage.isRequiredErrorMessageVisible(),"Incorrect Results");
+    }
+
+    @Test
+    public void testEmptyEmail()
+    {
+        //EnterInvalidUserName
+        loginPage.setUserNameField("");
+        //EnterValidPassword
+        loginPage.setPasswordField("admin123");
+        //ClickButton
+        loginPage.clickLoginButton();
+
+        //Assertion
+        assertTrue(loginPage.isRequiredErrorMessageVisible(),"Incorrect Results");
+    }
+
+    @Test
+    public void testEmptyEmailAndPassword()
+    {
+        //EnterInvalidUserName
+        loginPage.setUserNameField("");
+        //EnterValidPassword
+        loginPage.setPasswordField("");
+        //ClickButton
+        loginPage.clickLoginButton();
+
+        //Assertion
+        assertTrue(loginPage.isDuplicatedRequiredErrorMessageVisible());
     }
 }
